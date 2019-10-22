6c9565de6ed02e074c55f58ac6f03514e2eb53ed
ando <k-anod@tees-con.co.jp>
2019-10-22 19:29:05 +0900

    デバイスごとに仕分けする仕組みの追加

--- MvcBasic/Global.asax.cs
+++ MvcBasic/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace MvcBasic
 {
@@ -19,9 +20,17 @@ namespace MvcBasic
             //データベース初期化設定
             Database.SetInitializer<MvcBasicContext>(new MvcBasicInitializer());
 
-            //ビューエンジンをRazorだけに（実行速度が少し上がる）
+            //ビューエンジンをRazorだけにする（実行速度が少し上がる）
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
+
+            //IPhone専用ページに飛ばす処理を登録
+            DisplayModeProvider.Instance.Modes.Insert(0,
+                new DefaultDisplayMode("iPhone")
+                {
+                    ContextCondition = c =>
+                        c.Request.UserAgent.Contains("iPhone")
+                });
         }
     }
 }
