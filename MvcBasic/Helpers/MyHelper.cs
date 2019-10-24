cdb54592e99869eefd45a81ccdb37e1c40902cf1
andou <k-ando@tees-con.co.jp>
2019-10-24 10:17:32 +0900

    C#クラスを利用した自作ヘルパーの作成

---
+++ MvcBasic/Helpers/MyHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasic.Helpers
+{
+    public class MyHelper
+    {
+        public static IHtmlString Mailto(string address, string linktext)
+        {
+            address = HttpUtility.HtmlAttributeEncode(address);
+            linktext = HttpUtility.HtmlAttributeEncode(linktext);
+
+            var tag = $@"<a href=""mailto:{address}"">{linktext}</a>";
+
+            return MvcHtmlString.Create(tag);
+        }
+    }
+}
